6a4bed616705d301b55f998d8f270a95da1fe0b8
Cuong Dang <cuong.developer19@gmail.com>
2021-08-18 09:12:45 +0700

    End of section 25.

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,5 +49,20 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message, data });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://john:Abcd1234@cluster0.pkegc.mongodb.net/messages?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/posts', feedController.getPosts);
-router.post('/posts', feedController.createPost);
+router.get('/posts', isAuth, feedController.getPosts);
+
+router.post(
+  '/posts',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  isAuth,
+  feedController.createPost
+);
+
+router.get('/posts/:postId', feedController.getPost);
+
+router.put(
+  '/posts/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  isAuth,
+  feedController.updatePost
+);
+
+router.delete('/posts/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
